bb3a769338f279dff3d6665abdb79bf154d327d9
root <hhthuongbtr@gmail.com>
2017-07-25 15:45:05 +0700

    system

--- system/urls.py
+++ system/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^datetime/$', views.datetime_json, name='datetime_json'),
-    url(r'^mountpoint/$', views.mountpoint_list_json, name='mountpoint_json'),
+    url(r'^api/datetime/$', views.datetime_json, name='datetime_json'),
+    url(r'^api/mountpoint/$', views.mountpoint_list_json, name='mountpoint_json'),
 ]
--- system/views.py
+++ system/views.py
@@ -19,7 +19,13 @@ def datetime_json(request):
     date_time = Thomson().get_datetime()
     return HttpResponse(date_time, content_type='application/json', status=200)
 
+def get_datetime(request):
+	return render_to_response('system/datetime.html')
+
 def mountpoint_list_json(request):
     mountpoint_list = Thomson().get_mountpoint()
     return HttpResponse(mountpoint_list, content_type='application/json', status=200)
+
+ def get_mountpoint(request):
+ 	return render_to_response('system/mountpoint.html')
 
