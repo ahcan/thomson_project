8735bef85b512d2917518dc4559497afb24fd44a
root <hhthuongbtr@gmail.com>
2017-07-25 15:55:19 +0700

    system

--- system/urls.py
+++ system/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     url(r'^api/datetime/$', views.datetime_json, name='datetime_json'),
     url(r'^api/mountpoint/$', views.mountpoint_list_json, name='mountpoint_json'),
+
+    url(r'^datetime/$', views.get_datetime, name='datetime'),
+    url(r'^mountpoint/$', views.get_datetime, name='mountpoint'),
 ]
