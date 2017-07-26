ebb4ef1242c2d5a1ea0917980852a030901911e8
root <hhthuongbtr@gmail.com>
2017-07-26 14:37:27 +0700

    fix url

--- log/urls.py
+++ log/urls.py
@@ -3,10 +3,10 @@ from . import views
 
 urlpatterns = [
     url(r'^api/log$', views.log_list_json, name='log_json'),
-    url(r'^api/open$', views.log_list_json, name='open_log_json'),
-    url(r'^api/(?P<jobID>\d+)/$', views.log_list_json, name='jobID_log_json'),
+    url(r'^api/open$', views.open_log_list_json, name='open_log_json'),
+    url(r'^api/(?P<jobID>\d+)/$', views.log_by_jobID_list_json, name='jobID_log_json'),
 
     url(r'^$', views.get_log, name='log'),
     url(r'^open/$', views.get_open_log, name='open'),
-    url(r'^(?P<job_id>\d+)/$', views.get_log_by_jobID, name='byjobid'),
+    url(r'^(?d<job_id>\d+)/$', views.get_log_by_jobID, name='byjobid'),
 ]
