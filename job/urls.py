892bc2e2cd9c548b22b967adb05a1b214ed25439
root <hhthuongbtr@gmail.com>
2017-07-26 12:34:07 +0700

    multi job

--- job/urls.py
+++ job/urls.py
@@ -3,6 +3,16 @@ from . import views
 
 urlpatterns = [
     url(r'^api/job/$', views.job_json, name='job_json'),
+    url(r'^api/waiting/$', views.waiting_job_json, name='waiting_job_json'),
+    url(r'^api/running/$', views.running_job_json, name='running_job_json'),
+    url(r'^api/paused/$', views.paused_job_json, name='paused_job_json'),
+    url(r'^api/completed/$', views.completed_job_json, name='completed_job_json'),
+    url(r'^api/aborted/$', views.aborted_job_json, name='aborted_job_json'),
 
     url(r'^$', views.get_job, name='job'),
+    url(r'^waiting/$', views.get_waiting_job, name='waiting'),
+    url(r'^running/$', views.get_running_job, name='running'),
+    url(r'^paused/$', views.get_paused_job, name='paused'),
+    url(r'^completed/$', views.get_completed_job, name='completed'),
+    url(r'^aborted/$', views.aborted_job_json, name='aborted'),
 ]
--- job/views.py
+++ job/views.py
@@ -10,14 +10,80 @@ from setting.get_thomson_api import *
 
 # Create your views here.
 
-#######################################################################
-#                                                                     #
-#------------------------------WORKFLOW-------------------------------#
-#                                                                     #
-#######################################################################
+##############################################################################
+#                                                                            #
+#-------------------------------------ALL JOB--------------------------------#
+#                                                                            #
+##############################################################################
 def job_json(request):
     date_time = Job().get_job()
     return HttpResponse(date_time, content_type='application/json', status=200)
 
 def get_job(request):
 	return render_to_response('job/job.html')
+
+#############################################################################
+#                                                                           #
+#------------------------------------WAITING--------------------------------#
+#                                                                           #
+#############################################################################
+
+def waiting_job_json(request):
+    date_time = Job().get_Waiting()
+    return HttpResponse(date_time, content_type='application/json', status=200)
+
+def get_waiting_job(request):
+	return render_to_response('job/job_waiting.html')
+
+##############################################################################
+#                                                                            #
+#------------------------------------RUNNING---------------------------------#
+#                                                                            #
+##############################################################################
+
+def running_job_json(request):
+    date_time = Job().get_Running()
+    return HttpResponse(date_time, content_type='application/json', status=200)
+
+def get_running_job(request):
+	return render_to_response('job/job_running.html')
+
+##############################################################################
+#                                                                            #
+#-----------------------------------PAUSED-----------------------------------#
+#                                                                            #
+##############################################################################
+
+def paused_job_json(request):
+    date_time = Job().get_Paused()
+    return HttpResponse(date_time, content_type='application/json', status=200)
+
+def get_paused_job(request):
+	return render_to_response('job/job_paused.html')
+
+##############################################################################
+#                                                                            #
+#----------------------------------COMPLETED---------------------------------#
+#                                                                            #
+##############################################################################
+
+def completed_job_json(request):
+    date_time = Job().get_Completed()
+    return HttpResponse(date_time, content_type='application/json', status=200)
+
+def get_completed_job(request):
+	return render_to_response('job/job_completed.html')
+
+##############################################################################
+#                                                                            #
+#-----------------------------------ABORTED----------------------------------#
+#                                                                            #
+##############################################################################
+
+def aborted_job_json(request):
+    date_time = Job().get_Aborted()
+    return HttpResponse(date_time, content_type='application/json', status=200)
+
+def get_aborted_job(request):
+	return render_to_response('job/job_aborted.html')
+	
