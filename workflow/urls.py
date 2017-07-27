03822da5e9bfd8e598724c105cfbb4229d4cceca
root <hhthuongbtr@gmail.com>
2017-07-27 14:27:57 +0700

    edit clock url

--- workflow/urls.py
+++ workflow/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     url(r'^api/workflow/$', views.workflow_json, name='workflow_json'),
     url(r'^$', views.get_workflow, name='workflow'),
-    url(r'^$', views.test_clock, name='clock'),
+    url(r'^clock/$', views.test_clock, name='clock'),
 ]
