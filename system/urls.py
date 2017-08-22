1000d73ced6c7bff8e52cfe9b07f9b919b7d19af
root <hhthuongbtr@gmail.com>
2017-08-22 15:04:55 +0700

    add system status /system/status/, /system/api/status, /system/bladestatus/

--- system/urls.py
+++ system/urls.py
@@ -4,7 +4,11 @@ from . import views
 urlpatterns = [
     url(r'^api/datetime/$', views.datetime_json, name='datetime_json'),
     url(r'^api/mountpoint/$', views.mountpoint_list_json, name='mountpoint_json'),
+    url(r'^api/status/$', views.get_system_status_json, name='status_json'),
+    url(r'^api/blade_status/$', views.get_blade_status_json, name='bladestatus_json'),
 
     url(r'^datetime/$', views.get_datetime, name='datetime'),
     url(r'^mountpoint/$', views.get_mountpoint, name='mountpoint'),
+    url(r'^status/$', views.get_system_status, name='status'),
+    url(r'^bladestatus/$', views.get_blade_status, name='bladestatus'),
 ]
