f81ccbeec16faf8698fdbaf4a4acc9bdd189540b
root <hhthuongbtr@gmail.com>
2017-08-29 14:12:35 +0700

    clear template

--- system/urls.py
+++ system/urls.py
@@ -2,17 +2,15 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    #Link get thomson datetime: /system/api/datetime
     url(r'^api/datetime/$', views.datetime_json, name='datetime_json'),
+    #Link get thomson datetime: /system/api/mountpoint
     url(r'^api/mountpoint/$', views.mountpoint_list_json, name='mountpoint_json'),
+    #Link get thomson info CPU, RAAM: /system/api/status
     url(r'^api/status/$', views.get_system_status_json, name='status_json'),
-    url(r'^api/bladestatus/$', views.get_blade_status_json, name='bladestatus_json'),
+    #Link get job status: /system/api/jobstatus
     url(r'^api/jobstatus/$', views.get_job_status_json, name='jobstatus_json'),
 
-    url(r'^datetime/$', views.get_datetime, name='datetime'),
-    url(r'^mountpoint/$', views.get_mountpoint, name='mountpoint'),
-    url(r'^status/$', views.get_system_status, name='status'),
-    url(r'^bladestatus/$', views.get_blade_status, name='bladestatus'),
-    url(r'^jobstatus/$', views.get_job_status, name='jobstatus'),
-
+    #Main template dashboard
     url(r'^$', views.get_system, name="system"),
 ]
--- system/views.py
+++ system/views.py
@@ -16,38 +16,26 @@ from setting.get_thomson_api import *
 #                                                                     #
 #######################################################################
 
+#Main template dashboard /system
 def get_system(request):
 	return render_to_response('system/system.html')
+
+#Link get thomson datetime: /system/api/datetime
 def datetime_json(request):
     date_time = Thomson().get_datetime()
     return HttpResponse(date_time, content_type='application/json', status=200)
 
-def get_datetime(request):
-	return render_to_response('system/datetime.html')
-
+#Link get thomson datetime: /system/api/mountpoint
 def mountpoint_list_json(request):
     mountpoint_list = Thomson().get_mountpoint()
     return HttpResponse(mountpoint_list, content_type='application/json', status=200)
 
-def get_mountpoint(request):
- 	return render_to_response('system/mountpoint.html')
-
+#Link get thomson info CPU, RAAM: /system/api/status
 def get_system_status_json(request):
 	system_status = Thomson().get_system_status()
 	return HttpResponse(system_status, content_type='application/json', status=200)
 
-def get_system_status(request):
- 	return render_to_response('system/system_status.html')
-
-def get_blade_status_json(request):
-	pass
-
-def get_blade_status(request):
- 	return render_to_response('system/blade_status.html')
-
+#Link get job status: /system/api/jobstatus
 def get_job_status_json(request):
 	job_status = Thomson().get_job_status()
 	return HttpResponse(job_status, content_type='application/json', status=200)
-
-def get_job_status(request):
- 	return render_to_response('system/job_status.html')
