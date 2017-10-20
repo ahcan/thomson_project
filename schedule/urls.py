981e8c9f2f493e22dbb3f5e75c2e6655578bdb22
Nguyen Huy <tronghuy02@gmail.com>
2017-10-20 03:15:46 -0400

    edit urls

--- schedule/urls.py
+++ schedule/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     #link response all crontab data: /crontabSMP/api/crontabSMP
     url(r'^api/schedule/$', views.get_schedule_json, name='schedule_api'),
     url(r'^add/$', views.get_add, name="add_job"),
+    url(r'^delete/(?P<id>[0-9]+)/$',views.remove_schedule, name='delete_schedule'),
     url(r'^$', views.get_index, name="index"), 
 ]
