04dff5ef6e97c3ec0b297b026c310bf80bb272f9
root <hhthuongbtr@gmail.com>
2017-11-17 16:25:17 +0700

    restorse link api /log/api/11659/

--- log/urls.py
+++ log/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     url(r'^api/log$', views.get_log_list_json, name='log_api'),
+    url(r'^api/(?P<job_id>\d+)/$', views.get_log_by_jobID_list_json, name='jobID_log_api'),
 
     #link show all logs template
     url(r'^$', views.get_log, name='log'),
-
 ]
