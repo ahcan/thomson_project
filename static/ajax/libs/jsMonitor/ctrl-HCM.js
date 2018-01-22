d09cc5e58ac8ef1625d849bb5f7e266268f2b2f3
huy <huy@localhost.localdomain>
2018-01-22 06:09:47 -0500

    fix reload log Job HCM

--- static/ajax/libs/jsMonitor/ctrl-HCM.js
+++ static/ajax/libs/jsMonitor/ctrl-HCM.js
@@ -116,6 +116,7 @@ app.controller('ctrl-thomson-HCM',function($scope, $http, $timeout, $window, $in
     $scope.showLog = function(job_id, job_name){
         $scope.job_name = job_name;
         $scope.isRealTime = true;
+        $scope.job_id = job_id;
         $timeout(function() { $scope.$broadcast('loadLog-HCM');}, 0);
         $http({
             method: 'GET',
@@ -148,7 +149,7 @@ app.controller('ctrl-thomson-HCM',function($scope, $http, $timeout, $window, $in
                 $scope.$broadcast('loadLog-HCM');
             } 
         });
-        if($scope.isJob && $scope.isRealTime){
+        if($scope.isJob && $scope.isRealTime && $scope.job_id == job_id){
             $timeout(function(){$scope.reload_log(job_id, job_name);}, 10000);
         }
     };
