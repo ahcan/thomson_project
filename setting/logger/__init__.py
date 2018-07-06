a473bd6ccb2047aceeedfbd600bfc7167dc98ac8
Trong Huy <tronghuy02@gmail.com>
2018-07-06 15:22:43 +0700

    change setting logger

--- setting/logger/__init__.py
+++ setting/logger/__init__.py
@@ -6,9 +6,10 @@ import json
 # from config.config import LOGGING as logging_config_dict
 
 
-with open("setting/logger/logging_configuration.json", 'r') as configuration_file:
-    config_dict = json.load(configuration_file)
-logging.config.dictConfig(config_dict)
+def getLogger(loggerName):
+    with open("setting/logger/logging_configuration.json", 'r') as configuration_file:
+        config_dict = json.load(configuration_file)
+    logging.config.dictConfig(config_dict)
 # Create the Logger
-logger = logging.getLogger(__name__)
+    return logging.getLogger(loggerName)
 # logger.setLevel(logging.WARNING)
